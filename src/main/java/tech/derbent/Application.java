5914a0c00590a1fbb520f7115f27088946e772a2
yasin <yasin@x1yy>
2025-07-12 23:35:53 +0300

    added eclipse formatting common files. file names points to settings locations

--- src/main/java/tech/derbent/Application.java
+++ src/main/java/tech/derbent/Application.java
@@ -1,24 +1,24 @@
 package tech.derbent;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
+import java.time.Clock;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.time.Clock;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 
 @SpringBootApplication
 @Theme("default")
 public class Application implements AppShellConfigurator {
 
-    @Bean
-    public Clock clock() {
-        return Clock.systemDefaultZone(); // You can also use Clock.systemUTC()
-    }
+	public static void main(final String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
+	@Bean
+	public Clock clock() {
+		return Clock.systemDefaultZone(); // You can also use Clock.systemUTC()
+	}
 }
