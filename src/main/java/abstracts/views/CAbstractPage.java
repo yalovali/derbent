04b10b81b522aea147e02a2ee9502c797a73d06b
yasin <yasin@x1yy>
2025-07-13 11:12:13 +0300

    added abstractview

---
+++ src/main/java/abstracts/views/CAbstractPage.java
@@ -0,0 +1,26 @@
+package views;
+
+import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
+public abstract class CAbstractPage extends Main {
+
+	private static final long serialVersionUID = 1L;
+
+	public CAbstractPage() {
+		super();
+		setSizeFull();
+		addClassNames(LumoUtility.BoxSizing.BORDER, LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN, LumoUtility.Padding.MEDIUM, LumoUtility.Gap.SMALL);
+		setupToolbar();
+	}
+
+	/**
+	 * Initializes the page with necessary components and layout.
+	 */
+	protected abstract void initPage();
+
+	/**
+	 * Sets up the toolbar for the page.
+	 */
+	protected abstract void setupToolbar();
+}
