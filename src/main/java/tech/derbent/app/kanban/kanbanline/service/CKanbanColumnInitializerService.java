e2ca197d9b2820e7ba8635d1bb81543180bf1e64
Yalovali <yalovali@gmail.com>
2025-12-21 03:22:03 +0300

    Add kanban column initializer

--- src/main/java/tech/derbent/app/kanban/kanbanline/service/CKanbanColumnInitializerService.java
+++ src/main/java/tech/derbent/app/kanban/kanbanline/service/CKanbanColumnInitializerService.java
@@ -3,9 +3,6 @@ package tech.derbent.app.kanban.kanbanline.service;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import tech.derbent.api.config.CSpringContext;
-import tech.derbent.api.entityOfCompany.service.CEntityOfCompanyService;
-import tech.derbent.api.registry.CEntityRegistry;
 import tech.derbent.api.screens.domain.CDetailSection;
 import tech.derbent.api.screens.domain.CGridEntity;
 import tech.derbent.api.screens.service.CDetailLinesService;
@@ -14,40 +11,43 @@ import tech.derbent.api.screens.service.CGridEntityService;
 import tech.derbent.api.screens.service.CInitializerServiceBase;
 import tech.derbent.api.screens.service.CInitializerServiceNamedEntity;
 import tech.derbent.api.utils.Check;
-import tech.derbent.app.companies.domain.CCompany;
+import tech.derbent.app.kanban.kanbanline.domain.CKanbanColumn;
 import tech.derbent.app.kanban.kanbanline.domain.CKanbanLine;
 import tech.derbent.app.page.service.CPageEntityService;
 import tech.derbent.app.projects.domain.CProject;
 
-public class CKanbanLineInitializerService extends CInitializerServiceBase {
-	private static final Class<?> clazz = CKanbanLine.class;
-	private static final Logger LOGGER = LoggerFactory.getLogger(CKanbanLineInitializerService.class);
-	private static final String menuOrder = Menu_Order_SETUP + ".90";
-	private static final String menuTitle = MenuTitle_SETUP + ".Kanban Lines";
-	private static final String pageDescription = "Kanban line definitions and their columns";
-	private static final String pageTitle = "Kanban Lines";
-	private static final boolean showInQuickToolbar = true;
+/** CKanbanColumnInitializerService - Initializes screen metadata and sample data for Kanban columns. */
+public final class CKanbanColumnInitializerService extends CInitializerServiceBase {
+
+	private static final Class<?> clazz = CKanbanColumn.class;
+	private static final Logger LOGGER = LoggerFactory.getLogger(CKanbanColumnInitializerService.class);
+	private static final String menuOrder = Menu_Order_SETUP + ".95";
+	private static final String menuTitle = MenuTitle_SETUP + ".Kanban Columns";
+	private static final String pageDescription = "Kanban column definitions for Kanban line configuration";
+	private static final String pageTitle = "Kanban Columns";
+	private static final boolean showInQuickToolbar = false;
 
 	public static CDetailSection createBasicView(final CProject project) throws Exception {
+		Check.notNull(project, "Project cannot be null");
 		try {
 			final CDetailSection detailSection = createBaseScreenEntity(project, clazz);
 			CInitializerServiceNamedEntity.createBasicView(detailSection, clazz, project, true);
-			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "company"));
-			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "kanbanColumns"));
+			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "itemOrder"));
+			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "kanbanLine"));
 			detailSection.addScreenLine(CDetailLinesService.createSection("Audit"));
 			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "createdDate"));
 			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "lastModifiedDate"));
 			detailSection.debug_printScreenInformation();
 			return detailSection;
 		} catch (final Exception e) {
-			LOGGER.error("Error creating Kanban line view.");
+			LOGGER.error("Error creating kanban column view.");
 			throw e;
 		}
 	}
 
 	public static CGridEntity createGridEntity(final CProject project) {
 		final CGridEntity grid = createBaseGridEntity(project, clazz);
-		grid.setColumnFields(List.of("id", "name", "description", "company", "active"));
+		grid.setColumnFields(List.of("id", "name", "description", "itemOrder", "kanbanLine", "active"));
 		return grid;
 	}
 
@@ -59,19 +59,28 @@ public class CKanbanLineInitializerService extends CInitializerServiceBase {
 				pageDescription, showInQuickToolbar, menuOrder);
 	}
 
-	public static void initializeSample(final CCompany company, final boolean minimal) throws Exception {
-		final String[][] sampleLines = {
+	public static void initializeSample(final CKanbanLine line, final int sampleIndex, final boolean minimal) {
+		Check.notNull(line, "Kanban line cannot be null when seeding columns");
+		final String[][] minimalSamples = {
 				{
-						"Default Kanban", "Backlog to done overview"
+						"Backlog", "Done"
 				}, {
-						"Team Swimlanes", "Single row Kanban board per team"
+						"To Do", "Done"
 				}
 		};
-		initializeCompanyEntity(sampleLines, (CEntityOfCompanyService<?>) CSpringContext.getBean(CEntityRegistry.getServiceClassForEntity(clazz)),
-				company, minimal, (entity, index) -> {
-					Check.instanceOf(entity, CKanbanLine.class, "Expected Kanban line for column initialization");
-					final CKanbanLine line = (CKanbanLine) entity;
-					CKanbanColumnInitializerService.initializeSample(line, index, minimal);
-				});
+		final String[][] fullSamples = {
+				{
+						"Backlog", "In Progress", "Done"
+				}, {
+						"To Do", "Doing", "Review", "Done"
+				}
+		};
+		final String[][] sampleSets = minimal ? minimalSamples : fullSamples;
+		final int index = Math.max(0, Math.min(sampleIndex, sampleSets.length - 1));
+		for (final String columnName : sampleSets[index]) {
+			line.addKanbanColumn(new CKanbanColumn(columnName, line));
+		}
 	}
+
+	private CKanbanColumnInitializerService() {}
 }
