e48ba41e1d8e0b7fc45d41f604cef443a39143cd
yasin <yasin@burhaniye>
2026-01-13 16:04:28 +0300

    refactor(bab): Simplify CBabDeviceInitializerService by removing unused imports and consolidating node creation logic

--- src/main/java/tech/derbent/bab/device/service/CBabDeviceInitializerService.java
+++ src/main/java/tech/derbent/bab/device/service/CBabDeviceInitializerService.java
@@ -1,11 +1,12 @@
 package tech.derbent.bab.device.service;
 
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import tech.derbent.api.companies.domain.CCompany;
 import tech.derbent.api.config.CSpringContext;
+import tech.derbent.api.page.service.CPageEntityService;
+import tech.derbent.api.projects.domain.CProject;
 import tech.derbent.api.registry.CEntityRegistry;
 import tech.derbent.api.screens.domain.CDetailSection;
 import tech.derbent.api.screens.domain.CGridEntity;
@@ -14,9 +15,6 @@ import tech.derbent.api.screens.service.CDetailSectionService;
 import tech.derbent.api.screens.service.CGridEntityService;
 import tech.derbent.api.screens.service.CInitializerServiceBase;
 import tech.derbent.api.screens.service.CInitializerServiceNamedEntity;
-import tech.derbent.api.page.service.CPageEntityService;
-import tech.derbent.api.projects.domain.CProject;
-import tech.derbent.api.companies.domain.CCompany;
 import tech.derbent.api.utils.Check;
 import tech.derbent.bab.device.domain.CBabDevice;
 import tech.derbent.bab.node.domain.CBabNodeCAN;
@@ -25,158 +23,137 @@ import tech.derbent.bab.node.domain.CBabNodeModbus;
 import tech.derbent.bab.node.domain.CBabNodeROS;
 import tech.derbent.bab.node.service.CBabNodeService;
 
-/**
- * Initializer service for BAB device.
- * Following Derbent pattern: createBasicView, createGridEntity, initialize, initializeSample.
- */
+/** Initializer service for BAB device. Following Derbent pattern: createBasicView, createGridEntity, initialize, initializeSample. */
 public class CBabDeviceInitializerService extends CInitializerServiceBase {
 
-private static final Class<?> clazz = CBabDevice.class;
-private static final Logger LOGGER = LoggerFactory.getLogger(CBabDeviceInitializerService.class);
-private static final String menuOrder = Menu_Order_SYSTEM + ".1";
-private static final String menuTitle = MenuTitle_SYSTEM + ".Devices";
-private static final String pageDescription = "IoT gateway device management and configuration";
-private static final String pageTitle = "Device Management";
-private static final boolean showInQuickToolbar = true;
+	private static final Class<?> clazz = CBabDevice.class;
+	private static final Logger LOGGER = LoggerFactory.getLogger(CBabDeviceInitializerService.class);
+	private static final String menuOrder = Menu_Order_SYSTEM + ".1";
+	private static final String menuTitle = MenuTitle_SYSTEM + ".Devices";
+	private static final String pageDescription = "IoT gateway device management and configuration";
+	private static final String pageTitle = "Device Management";
+	private static final boolean showInQuickToolbar = true;
 
-public static CDetailSection createBasicView(final CProject project) throws Exception {
-Check.notNull(project, "project cannot be null");
-try {
-final CDetailSection detailSection = createBaseScreenEntity(project, clazz);
-CInitializerServiceNamedEntity.createBasicView(detailSection, clazz, project, true);
+	public static CDetailSection createBasicView(final CProject project) throws Exception {
+		Check.notNull(project, "project cannot be null");
+		try {
+			final CDetailSection detailSection = createBaseScreenEntity(project, clazz);
+			CInitializerServiceNamedEntity.createBasicView(detailSection, clazz, project, true);
+			detailSection.addScreenLine(CDetailLinesService.createSection("Device Information"));
+			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "serialNumber"));
+			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "firmwareVersion"));
+			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "hardwareRevision"));
+			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "deviceStatus"));
+			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "lastSeen"));
+			detailSection.addScreenLine(CDetailLinesService.createSection("Network Configuration"));
+			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "ipAddress"));
+			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "macAddress"));
+			detailSection.addScreenLine(CDetailLinesService.createSection("System"));
+			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "company"));
+			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "createdBy"));
+			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "active"));
+			detailSection.addScreenLine(CDetailLinesService.createSection("Audit"));
+			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "createdDate"));
+			detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "lastModifiedDate"));
+			detailSection.debug_printScreenInformation();
+			return detailSection;
+		} catch (final Exception e) {
+			LOGGER.error("Error creating device view.", e);
+			throw e;
+		}
+	}
 
-detailSection.addScreenLine(CDetailLinesService.createSection("Device Information"));
-detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "serialNumber"));
-detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "firmwareVersion"));
-detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "hardwareRevision"));
-detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "deviceStatus"));
-detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "lastSeen"));
+	public static CGridEntity createGridEntity(final CProject project) {
+		final CGridEntity grid = createBaseGridEntity(project, clazz);
+		grid.setColumnFields(List.of("id", "name", "description", "serialNumber", "firmwareVersion", "hardwareRevision", "deviceStatus", "lastSeen",
+				"ipAddress", "macAddress", "company", "createdBy", "active", "createdDate", "lastModifiedDate"));
+		return grid;
+	}
 
-detailSection.addScreenLine(CDetailLinesService.createSection("Network Configuration"));
-detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "ipAddress"));
-detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "macAddress"));
+	private static void createSampleNodes(final CBabDevice device, final CBabNodeService nodeService, final boolean minimal) {
+		// Create CAN node
+		final CBabNodeCAN canNode = new CBabNodeCAN("CAN Bus Interface", device);
+		canNode.setDescription("Primary CAN bus interface for vehicle communication");
+		canNode.setBitrate(500000);
+		canNode.setSamplePoint(0.875);
+		canNode.setInterfaceName("can0");
+		canNode.setEnabled(true);
+		canNode.setNodeStatus("Active");
+		nodeService.save(canNode);
+		LOGGER.info("Created CAN node: {}", canNode.getName());
+		// Create Ethernet node
+		final CBabNodeEthernet ethNode = new CBabNodeEthernet("Ethernet Interface", device);
+		ethNode.setDescription("Primary Ethernet interface for network communication");
+		ethNode.setInterfaceName("eth0");
+		ethNode.setIpAddress("192.168.1.100");
+		ethNode.setSubnetMask("255.255.255.0");
+		ethNode.setGateway("192.168.1.1");
+		ethNode.setDhcpEnabled(false);
+		ethNode.setEnabled(true);
+		ethNode.setNodeStatus("Active");
+		nodeService.save(ethNode);
+		LOGGER.info("Created Ethernet node: {}", ethNode.getName());
+		if (!minimal) {
+			// Create Modbus node
+			final CBabNodeModbus modbusNode = new CBabNodeModbus("Modbus RTU Interface", device);
+			modbusNode.setDescription("Modbus RTU interface for industrial sensors");
+			modbusNode.setProtocolType("RTU");
+			modbusNode.setSlaveId(1);
+			modbusNode.setBaudRate(9600);
+			modbusNode.setParity("None");
+			modbusNode.setEnabled(false);
+			modbusNode.setNodeStatus("Inactive");
+			nodeService.save(modbusNode);
+			LOGGER.info("Created Modbus node: {}", modbusNode.getName());
+			// Create ROS node
+			final CBabNodeROS rosNode = new CBabNodeROS("ROS Bridge", device);
+			rosNode.setDescription("ROS bridge for robot communication");
+			rosNode.setRosMasterUri("http://192.168.1.101:11311");
+			rosNode.setNodeName("bab_gateway");
+			rosNode.setNamespace("/gateway");
+			rosNode.setRosVersion("ROS1");
+			rosNode.setEnabled(false);
+			rosNode.setNodeStatus("Inactive");
+			nodeService.save(rosNode);
+			LOGGER.info("Created ROS node: {}", rosNode.getName());
+		}
+	}
 
-detailSection.addScreenLine(CDetailLinesService.createSection("System"));
-detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "company"));
-detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "createdBy"));
-detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "active"));
+	public static void initialize(final CProject project, final CGridEntityService gridEntityService,
+			final CDetailSectionService detailSectionService, final CPageEntityService pageEntityService) throws Exception {
+		final CDetailSection detailSection = createBasicView(project);
+		final CGridEntity grid = createGridEntity(project);
+		initBase(clazz, project, gridEntityService, detailSectionService, pageEntityService, detailSection, grid, menuTitle, pageTitle,
+				pageDescription, showInQuickToolbar, menuOrder);
+	}
 
-detailSection.addScreenLine(CDetailLinesService.createSection("Audit"));
-detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "createdDate"));
-detailSection.addScreenLine(CDetailLinesService.createLineFromDefaults(clazz, "lastModifiedDate"));
-
-detailSection.debug_printScreenInformation();
-return detailSection;
-} catch (final Exception e) {
-LOGGER.error("Error creating device view.", e);
-throw e;
-}
-}
-
-public static CGridEntity createGridEntity(final CProject project) {
-final CGridEntity grid = createBaseGridEntity(project, clazz);
-grid.setColumnFields(List.of("id", "name", "description", "serialNumber", "firmwareVersion", 
-"hardwareRevision", "deviceStatus", "lastSeen", "ipAddress", "macAddress", 
-"company", "createdBy", "active", "createdDate", "lastModifiedDate"));
-return grid;
-}
-
-public static void initialize(final CProject project, final CGridEntityService gridEntityService,
-final CDetailSectionService detailSectionService, final CPageEntityService pageEntityService) throws Exception {
-final CDetailSection detailSection = createBasicView(project);
-final CGridEntity grid = createGridEntity(project);
-initBase(clazz, project, gridEntityService, detailSectionService, pageEntityService, detailSection, grid, 
-menuTitle, pageTitle, pageDescription, showInQuickToolbar, menuOrder);
-}
-
-/**
- * Initialize sample BAB device with nodes.
- * Following Derbent pattern: uses project parameter, gets company from project.
- * 
- * @param project the project
- * @param minimal if true, create minimal sample data
- */
-public static void initializeSample(final CProject project, final boolean minimal) throws Exception {
-final CCompany company = project.getCompany();
-LOGGER.info("Initializing BAB sample data for company: {}", company.getName());
-
-final CBabDeviceService deviceService = (CBabDeviceService) CSpringContext.getBean(
-CEntityRegistry.getServiceClassForEntity(clazz));
-final CBabNodeService nodeService = (CBabNodeService) CSpringContext.getBean(CBabNodeService.class);
-
-// Create or get unique device for company
-CBabDevice device = deviceService.getUniqueDevice().orElse(null);
-
-if (device == null) {
-device = new CBabDevice("IoT Gateway Device", company);
-device.setSerialNumber("BAB-GW-" + System.currentTimeMillis());
-device.setFirmwareVersion("1.0.0");
-device.setHardwareRevision("Rev A");
-device.setDeviceStatus("Online");
-device.setIpAddress("192.168.1.100");
-device.setMacAddress("00:1A:2B:3C:4D:5E");
-device.setDescription("Primary IoT gateway device for CAN bus to Ethernet/ROS protocol conversion");
-device = deviceService.save(device);
-LOGGER.info("Created sample device: {}", device.getName());
-} else {
-LOGGER.info("Device already exists: {}", device.getName());
-}
-
-// Create sample nodes if device has no nodes
-if (nodeService.countByDevice(device) == 0) {
-createSampleNodes(device, nodeService, minimal);
-}
-}
-
-private static void createSampleNodes(final CBabDevice device, final CBabNodeService nodeService, final boolean minimal) {
-// Create CAN node
-final CBabNodeCAN canNode = new CBabNodeCAN("CAN Bus Interface", device);
-canNode.setDescription("Primary CAN bus interface for vehicle communication");
-canNode.setBitrate(500000);
-canNode.setSamplePoint(0.875);
-canNode.setInterfaceName("can0");
-canNode.setEnabled(true);
-canNode.setNodeStatus("Active");
-nodeService.save(canNode);
-LOGGER.info("Created CAN node: {}", canNode.getName());
-
-// Create Ethernet node
-final CBabNodeEthernet ethNode = new CBabNodeEthernet("Ethernet Interface", device);
-ethNode.setDescription("Primary Ethernet interface for network communication");
-ethNode.setInterfaceName("eth0");
-ethNode.setIpAddress("192.168.1.100");
-ethNode.setSubnetMask("255.255.255.0");
-ethNode.setGateway("192.168.1.1");
-ethNode.setDhcpEnabled(false);
-ethNode.setEnabled(true);
-ethNode.setNodeStatus("Active");
-nodeService.save(ethNode);
-LOGGER.info("Created Ethernet node: {}", ethNode.getName());
-
-if (!minimal) {
-// Create Modbus node
-final CBabNodeModbus modbusNode = new CBabNodeModbus("Modbus RTU Interface", device);
-modbusNode.setDescription("Modbus RTU interface for industrial sensors");
-modbusNode.setProtocolType("RTU");
-modbusNode.setSlaveId(1);
-modbusNode.setBaudRate(9600);
-modbusNode.setParity("None");
-modbusNode.setEnabled(false);
-modbusNode.setNodeStatus("Inactive");
-nodeService.save(modbusNode);
-LOGGER.info("Created Modbus node: {}", modbusNode.getName());
-
-// Create ROS node
-final CBabNodeROS rosNode = new CBabNodeROS("ROS Bridge", device);
-rosNode.setDescription("ROS bridge for robot communication");
-rosNode.setRosMasterUri("http://192.168.1.101:11311");
-rosNode.setNodeName("bab_gateway");
-rosNode.setNamespace("/gateway");
-rosNode.setRosVersion("ROS1");
-rosNode.setEnabled(false);
-rosNode.setNodeStatus("Inactive");
-nodeService.save(rosNode);
-LOGGER.info("Created ROS node: {}", rosNode.getName());
-}
-}
+	/** Initialize sample BAB device with nodes. Following Derbent pattern: uses project parameter, gets company from project.
+	 * @param project the project
+	 * @param minimal if true, create minimal sample data */
+	public static void initializeSample(final CProject project, final boolean minimal) throws Exception {
+		final CCompany company = project.getCompany();
+		LOGGER.info("Initializing BAB sample data for company: {}", company.getName());
+		final CBabDeviceService deviceService = (CBabDeviceService) CSpringContext.getBean(CEntityRegistry.getServiceClassForEntity(clazz));
+		final CBabNodeService nodeService = (CBabNodeService) CSpringContext.getBean(CBabNodeService.class);
+		// Create or get unique device for company
+		CBabDevice device = deviceService.getUniqueDevice().orElse(null);
+		if (device == null) {
+			device = new CBabDevice("IoT Gateway Device", company);
+			device.setSerialNumber("BAB-GW-" + System.currentTimeMillis());
+			device.setFirmwareVersion("1.0.0");
+			device.setHardwareRevision("Rev A");
+			device.setDeviceStatus("Online");
+			device.setIpAddress("192.168.1.100");
+			device.setMacAddress("00:1A:2B:3C:4D:5E");
+			device.setDescription("Primary IoT gateway device for CAN bus to Ethernet/ROS protocol conversion");
+			device = deviceService.save(device);
+			LOGGER.info("Created sample device: {}", device.getName());
+		} else {
+			LOGGER.info("Device already exists: {}", device.getName());
+		}
+		// Create sample nodes if device has no nodes
+		if (nodeService.countByDevice(device) == 0) {
+			createSampleNodes(device, nodeService, minimal);
+		}
+	}
 }
